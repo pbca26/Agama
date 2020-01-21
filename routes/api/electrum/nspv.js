2ea5205eb6538b9573b7561a4802ab7f95da92c8
pbca26 <pbca26@gmail.com>
2020-01-21 18:03:54 +0300

    npsv stopNSPVDaemon function

--- routes/api/electrum/nspv.js
+++ routes/api/electrum/nspv.js
@@ -5,9 +5,8 @@ const nspvPorts = require('./nspvPorts');
 module.exports = (api) => {
   api.nspvRequest = (coin, method, params) => {
     return new Promise((resolve, reject) => {
-      const nspvServerUrl = `http://localhost:${nspvPorts[coin.toUpperCase()]}/`;
       const options = {
-        url: nspvServerUrl,
+        url: `http://localhost:${nspvPorts[coin.toUpperCase()]}/`,
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -19,17 +18,37 @@ module.exports = (api) => {
         }),
       };
 
-      console.log(JSON.stringify({
+      api.log(JSON.stringify({
         jsonrpc: '2.0',
         method,
         params,
-      }));
+      }), 'spv.nspv.req');
 
       request(options, (error, response, body) => {
-        console.log(body);
+        api.log(body, 'spv.nspv.req');
         resolve(JSON.parse(body));
       });
     });
+  };
+
+  api.stopNSPVDaemon = (coin) => {
+    if (coin === 'all') {
+      for (let key in api.electrumCoins) {
+        if (api.electrumCoins[key].nspv &&
+            api.nspvProcesses[key].pid) {
+          api.log(`NSPV daemon ${key.toUpperCase()} PID ${api.nspvProcesses[key].pid} is stopped`, 'spv.nspv.coin');
+          api.nspvProcesses[key].process.kill('SIGHUP');
+          delete api.nspvProcesses[key];
+        }
+      }
+    } else {
+      if (api.electrumCoins[coin].nspv &&
+          api.nspvProcesses[coin].pid) {
+        api.log(`NSPV daemon ${coin.toUpperCase()} PID ${api.nspvProcesses[coin].pid} is stopped`, 'spv.nspv.coin');
+        api.nspvProcesses[coin].process.kill('SIGHUP');
+        delete api.nspvProcesses[coin];
+      }
+    }
   };
 
   return api;
